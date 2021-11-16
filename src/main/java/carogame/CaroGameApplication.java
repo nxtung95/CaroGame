1b9632e076b0217d07aab16290029c95258df040
tungnx <tungnx@vnpay.vn>
2021-11-16 17:30:17 +0700

    Commit

---
+++ src/main/java/carogame/CaroGameApplication.java
@@ -0,0 +1,13 @@
+package carogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaroGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaroGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/carogame/CaroGameApplicationTests.java
@@ -0,0 +1,13 @@
+package carogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaroGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
